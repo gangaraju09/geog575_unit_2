1e8e60c81fce9fa570f16eb1a16bfd6cad775c56
GRK09 <98504161+GRK09@users.noreply.github.com>
2023-03-06 18:06:36 -0600

    No changes are made to previous one

--- js/main.js
+++ js/main.js
@@ -70,13 +70,15 @@ function createMap(){
         zoom: 2
     });
 
+    
     //add base map tilelayer
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	maxZoom: 20
-    }).addTo(map);
+	maxZoom: 20,
+    }).addTo(map);    
 
+    
     //call getData function
     getData(map);
 
